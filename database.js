c5a8f6b7f45a382f14f64cf1f0dd5603b03e0f12
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-01-31 18:44:18 -0500

    Added env variables

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
-
+require('dotenv').config();
 const conn = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Raushan@0128',
-    database:'db2'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 module.exports = conn;
