4458092fa653adb1c59c673455987d10d3c38731
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-01-31 14:10:51 -0500

    Workflow query correction

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = require('./index');
+var mysql = require("mysql");
+
+const conn = require('./database');
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+
+conn.connect((err)=>{
+    try {
+        const createTable = `CREATE TABLE IF NOT EXISTS users(id int NOT NULL PRIMARY KEY AUTO_INCREMENT, \
+            first_name VARCHAR(255), \
+            last_name VARCHAR(255), \
+            password VARCHAR(255), \
+            username VARCHAR(255), \
+            account_created VARCHAR(255), \
+            account_updated VARCHAR(255)  \
+        );`
+        
+        conn.query(createTable, (err, res) => {
+            if (err) throw err;
+        })
+    } catch (err) {
+        console.error(err);
+    }
+});
