4b94288f3c12eed95244def626311cc48a83db14
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-01-31 13:07:39 -0500

    Corrected workflow test

---
+++ test.js
@@ -0,0 +1,17 @@
+let chai = require('chai');
+let chaiHTTP = require('chai-http');
+var should = chai.should();
+
+const app = require('./index');
+chai.use(chaiHTTP);
+
+describe('Test health', () => {
+    it('health should be 200 OK', (done) => {
+        chai.request(app)
+            .get('/healthz')
+            .end((err, res) => {
+                (res).should.have.status(200);
+                done();
+            });
+    });
+});
