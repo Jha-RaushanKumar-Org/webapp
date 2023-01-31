e81df058fbbad7cbed8f085da7e0f4800901cac5
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-01-31 14:10:51 -0500

    Workflow query correction

--- index.js
+++ index.js
@@ -9,32 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 const routes = require('./routes/route');
-
-routes(app);
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-
 app.get("/healthz",(req, res)=>{
     res.status(200).json();    
 });
 
-conn.connect((err)=>{
-    try {
-        const createTable = `CREATE TABLE IF NOT EXISTS users(id int NOT NULL PRIMARY KEY AUTO_INCREMENT, \
-            first_name VARCHAR(255), \
-            last_name VARCHAR(255), \
-            password VARCHAR(255), \
-            username VARCHAR(255), \
-            account_created VARCHAR(255), \
-            account_updated VARCHAR(255)  \
-        );`
-        
-        conn.query(createTable, (err, res) => {
-            if (err) throw err;
-        })
-    } catch (err) {
-        console.error(err);
-    }
-});
+routes(app);
+
+
 
 module.exports = app;
