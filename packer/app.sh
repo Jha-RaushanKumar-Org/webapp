9c209a240af8ad55c9e05e6fe2136c08b7dc63f1
Jha-RaushanKumar <jha.rau@northeastern.edu>
2023-02-23 03:29:39 -0500

    Action corr

--- packer/app.sh
+++ packer/app.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-sleep -30
+sleep 30
 sudo yum update -y
 sudo yum upgrade -y
 sudo amazon-linux-extras install nginx1.12 -y
-sudo yum install -y unzip
-
 
 sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
+
 
 sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
 wget http://dev.mysql.com/get/mysql57-community-release-el7-8.noarch.rpm
@@ -16,22 +15,22 @@ sudo yum localinstall -y mysql57-community-release-el7-8.noarch.rpm
 sudo yum install -y mysql-community-server
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
+
 sudo grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}' > /tmp/mysql.password
 MYSQL_ROOT_PASSWORD=$(cat /tmp/mysql.password)
 mysql -u root --password=$MYSQL_ROOT_PASSWORD --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Raushan@123';"
 
-sleep 10
 echo "##Unzipping of Application##"
 
+sudo yum install unzip -y
 cd ~/ && unzip webapp.zip
+cd ~/webapp && npm i 
 
-cd ~/webapp && npm i
-sleep 10
+ cd ~/webapp
 
-cd ~/webapp
- #set environment variables
  sudo touch .env
  sudo chmod 777 .env
+
  echo "DB_HOST=localhost" >> .env
  echo "DB_PORT=3000" >> .env
  echo "DB_USER=root" >> .env
